b89f59835db6173f06123b162137f345c578efcd
Roman <maderomk@yandex.ru>
2022-10-12 09:31:34 +0300

    Для Александра

---
+++ gateway/src/main/java/ru/practicum/shareit/exeption/ErrorHandler.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.exeption;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleUnsupportedStatusException(final MethodArgumentTypeMismatchException e) {
+        return new ValidationErrorResponse(String.format(e.getName(), e.getValue()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleConstraintViolationException(final ConstraintViolationException e) {
+        return new ValidationErrorResponse(e.getMessage());
+    }
+}
